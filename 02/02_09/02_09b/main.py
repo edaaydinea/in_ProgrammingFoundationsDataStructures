f0047ed788f7bcc44569d8d858eaf7ab89194efa
edaaydinea <edaaydin.ea@outlook.com>
2024-09-26 17:48:43 +0300

    Refactor find_second_smallest function in main.py

--- 02/02_09/02_09b/main.py
+++ 02/02_09/02_09b/main.py
@@ -1,4 +1,7 @@
 def find_second_smallest(my_list):
-    return 0
+    if len(my_list) < 2:
+        return None
+    sorted_list = sorted(my_list)
+    return sorted_list[1]
 
 print(find_second_smallest([5, 8, 3, 2, 6]))
