6edb4915e37b5fd5ec87d7cc717579e375134f8c
edaaydinea <edaaydin.ea@outlook.com>
2024-09-26 17:36:41 +0300

    Compute average number of pets per student in a given class

--- 02/02_02/02_02b/main.py
+++ 02/02_02/02_02b/main.py
@@ -2,3 +2,11 @@
 Problem Statement: Compute the average number of pets each student 
 has in a given class. 
 '''
+student_pet_count_list = [0, 1, 0, 2, 1, 1, 4, 0, 0, 0, 3, 2, 1, 3, 0, 2, 2, 4]
+
+NUM_OF_STUDENTS = len(student_pet_count_list)
+print(f"Number of students: {NUM_OF_STUDENTS}")
+
+# average number of pets
+average_pet_count = sum(student_pet_count_list) / NUM_OF_STUDENTS
+print(f"Average number of pets: {average_pet_count}")
